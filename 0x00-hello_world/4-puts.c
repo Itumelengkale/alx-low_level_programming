5d655f16064430ac5905cb833b66268075750ab3
Itumelengkale <itumelengkale65@gmail.com>
2023-03-20 10:54:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with push funtion
- * return: 0 (success)
- */
+ *main -print a line of code using put
+ *
+ * Return: 0
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
